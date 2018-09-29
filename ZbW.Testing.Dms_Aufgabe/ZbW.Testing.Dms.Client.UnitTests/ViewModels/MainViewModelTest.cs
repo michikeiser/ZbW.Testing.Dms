0026ebb477610d41ec9bcb0da9251aedc754d028
osja <oswald.jan@gmail.com>
2018-09-29 12:54:15 +0200

    Init Test Project

---
+++ ZbW.Testing.Dms_Aufgabe/ZbW.Testing.Dms.Client.UnitTests/ViewModels/MainViewModelTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZbW.Testing.Dms.Client.UnitTests.ViewModels {
+    [TestClass]
+    public class MainViewModelTest {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
